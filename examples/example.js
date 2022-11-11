9752c30ef1cdf80b282c8b4ad6186ff0361d1df4
chin <a5566qq2581@gmail.com>
2022-11-11 22:00:42 +0800

    feat: update example usage
    - Fix the path of `incorrectProfile`
    - Add `loadIg`, `getKnownIGs` example

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-const validator = require("../src/validator");
+const fhirValidator = require("../src/validator");
 
 const fs = require("fs");
 const path = require("path");
@@ -6,8 +6,10 @@ const path = require("path");
 
 
 (async () => {
-
+    let validator = new fhirValidator();
     try {
+        
+
         // validate resource example
         let item = fs.readFileSync(path.join(__dirname, "./Patient-pat-example-tw-1-incorrect.json"), "utf-8");
         let validationOperationOutcome = await validator.validateResource(item, "https://twcore.mohw.gov.tw/fhir/StructureDefinition/Patient-twcore");
@@ -17,15 +19,19 @@ const path = require("path");
         let profileStr = fs.readFileSync(path.join(__dirname, "../igs/StructureDefinition-us-core-careplan.json"), "utf-8");
         await validator.loadProfile(profileStr);
 
-        let incorrectProfile = fs.readFileSync(path.join(__dirname, "../igs/no-content.json"), "utf-8");
-        await validator.loadProfile(incorrectProfile);
-
         let structures = await validator.getStructures();
-
         if (structures.includes("http://hl7.org/fhir/us/core/StructureDefinition/us-core-careplan")) {
             console.log("load profile \"http://hl7.org/fhir/us/core/StructureDefinition/us-core-careplan\" from file successful");
         }
+
+        let incorrectProfile = fs.readFileSync(path.join(__dirname, "./no-content.json"), "utf-8");
+        await validator.loadProfile(incorrectProfile);
+        
     } catch (e) {
         console.error("err", e);
     }
+
+    await validator.loadIg("hl7.fhir.us.core", "4.0.0");
+    let knownIGs = await validator.getKnownIGs();
+    console.log(knownIGs);
 })();
