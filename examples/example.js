2ac0c043c21931eb52c3d75e553e310b28e91541
chin <a5566qq2581@gmail.com>
2023-07-27 22:31:21 +0800

    chore: update example.js

--- examples/example.js
+++ examples/example.js
@@ -1,37 +1,28 @@
-const fhirValidator = require("../src/validator");
+const {FhirValidator} = require("../src/validator");
 
 const fs = require("fs");
 const path = require("path");
 
 
-
 (async () => {
-    let validator = new fhirValidator();
+    let validator = new FhirValidator({
+        igDir: path.join(__dirname, "../igs")
+    });
     try {
         
 
         // validate resource example
-        let item = fs.readFileSync(path.join(__dirname, "./Patient-pat-example-tw-1-incorrect.json"), "utf-8");
-        let validationOperationOutcome = await validator.validateResource(item, "https://twcore.mohw.gov.tw/fhir/StructureDefinition/Patient-twcore");
-        console.log(validationOperationOutcome);
+        let errorItem = fs.readFileSync(path.join(__dirname, "../test/us-core-patient-example-error.json"), "utf-8");
+        let validationOperationOutcome = await validator.validate(errorItem, "http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient");
+        console.log(JSON.stringify(validationOperationOutcome, null, 4));
 
-
-        let profileStr = fs.readFileSync(path.join(__dirname, "../igs/StructureDefinition-us-core-careplan.json"), "utf-8");
-        await validator.loadProfile(profileStr);
-
-        let structures = await validator.getStructures();
-        if (structures.includes("http://hl7.org/fhir/us/core/StructureDefinition/us-core-careplan")) {
-            console.log("load profile \"http://hl7.org/fhir/us/core/StructureDefinition/us-core-careplan\" from file successful");
-        }
-
-        let incorrectProfile = fs.readFileSync(path.join(__dirname, "./no-content.json"), "utf-8");
-        await validator.loadProfile(incorrectProfile);
+        // validate resource with multiple profiles
+        // please using comma to concat profile URLs
+        let item = fs.readFileSync(path.join(__dirname, "../test/us-core-patient-example.json"), "utf-8");
+        let oo = await validator.validate(item, "http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient,http://hl7.org/fhir/StructureDefinition/Patient");
+        console.log(JSON.stringify(oo, null, 4));
         
     } catch (e) {
         console.error("err", e);
     }
-
-    await validator.loadIg("hl7.fhir.us.core", "4.0.0");
-    let knownIGs = await validator.getKnownIGs();
-    console.log(knownIGs);
 })();
